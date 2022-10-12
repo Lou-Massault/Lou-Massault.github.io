f96fb4a50d0edd641a452ed6617df4af6088077c
Lou-Massault <111302559+Lou-Massault@users.noreply.github.com>
2022-10-12 13:56:49 -0400

    update

--- labo2/script.js
+++ labo2/script.js
@@ -2,7 +2,14 @@ let liste_cartes = ["/labo2/images/1.png", "/labo2/images/2.png", "/labo2/images
 "/labo2/images/14.png", "/labo2/images/15.png", "/labo2/images/16.png", "/labo2/images/17.png", "/labo2/images/18.png", "/labo2/images/19.png", "/labo2/images/20.png", "/labo2/images/21.png", "/labo2/images/22.png", "/labo2/images/23.png", "/labo2/images/24.png", "/labo2/images/25.png", "/labo2/images/26.png",
 "/labo2/images/27.png", "/labo2/images/28.png", "/labo2/images/29.png", "/labo2/images/30.png", "/labo2/images/31.png", "/labo2/images/32.png", "/labo2/images/33.png", "/labo2/images/34.png", "/labo2/images/35.png", "/labo2/images/36.png", "/labo2/images/37.png", "/labo2/images/38.png", "/labo2/images/39.png",
 "/labo2/images/40.png", "/labo2/images/41.png", "/labo2/images/42.png", "/labo2/images/43.png", "/labo2/images/44.png", "/labo2/images/45.png", "/labo2/images/46.png", "/labo2/images/47.png", "/labo2/images/48.png", "/labo2/images/49.png", "/labo2/images/50.png", "/labo2/images/51.png", "/labo2/images/52.png"]
+let dos_carte = ["/labo2/images/carte_dos.png"]
 let len = liste_cartes.length
+
+function affichageDos() {
+    for (let i=0; i<len; i++) {
+        document.querySelector("#_"+(i+1)).src = dos_carte
+    }
+}
 
 function affichageDeck() {
     for (let i=0; i<len; i++) {
@@ -10,7 +17,7 @@ function affichageDeck() {
     }
 }
 
-window.addEventListener('load', affichageDeck)
+window.addEventListener('load', affichageDos)
 
 function brassage() {
     let len = liste_cartes.length
@@ -29,6 +36,8 @@ function brassage() {
 
 }
 
+let btn_affichage = document.querySelector('#btn_affichage')
+btn_affichage.addEventListener('click', affichageDeck)
 
-let btn = document.querySelector('#btn')
-btn.addEventListener('click', brassage)
+let btn_brassage = document.querySelector('#btn_brassage')
+btn_brassage.addEventListener('click', brassage)
